cf4f2ba7de195972c1e6b58b44cc17032f84a7a6
Gary Taylor <gary.taylor@hismessagegs.com>
2017-11-30 14:26:49 +0000

    Improved controller spec for calculator to isolate it from the calculator service

--- spec/controllers/api/calculator/calculations_controller_spec.rb
+++ spec/controllers/api/calculator/calculations_controller_spec.rb
@@ -3,6 +3,7 @@ module Api
   module Calculator
     RSpec.describe CalculationsController, type: :controller do
       let(:auth_token) { 'my-big-secret' }
+      let!(:calculator_service_class) { class_spy(::Calculator::CalculationService).as_stubbed_const }
 
       before do
         allow(Settings.submission).to receive(:token).and_return('my-big-secret')
@@ -12,6 +13,10 @@ module Api
 
       describe 'POST #create' do
         describe 'when sent the correct authentication header' do
+          let(:calculator_service) { instance_double(Calculator::CalculationService, failed?: false) }
+          before do
+            expect(calculator_service_class).to receive(:call).with(anything).and_return(calculator_service)
+          end
           subject(:returned) { response }
           let(:calculation_data) { { 'inputs' => {} } }
           it { is_expected.to have_http_status(:success) }
@@ -33,7 +38,7 @@ module Api
 
         describe 'when sent the incorrect authentication header' do
           subject { response }
-
+          let(:calculation_data) { { 'inputs' => {} } }
           let(:auth_token) { 'different-big-secret' }
 
           it { is_expected.to have_http_status(:unauthorized) }
---
+++ spec/services/calculator/disposable_calculation_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+module Calculator
+  RSpec.describe DisposableCalculationService do
+    subject(:service) { described_class }
+    describe '#call' do
+
+    end
+
+    describe '#failure?' do
+
+    end
+
+    describe '#failure_reasons' do
+
+    end
+  end
+end
